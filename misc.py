3dd313744af6a63bd94c1a49a2fdd8d7bdc6fe42
CCortello <cody.cortello@gmail.com>
2016-01-14 17:00:35 -0700

    Changed template settings, asserted PEP8 errors as warnings

--- misc.py
+++ misc.py
@@ -1,5 +1,16 @@
-#Author = CCortello
-#Date = 1/14/2016
+# Author = CCortello
+# Date = 1/14/2016
 
 """Random musings and messings-around."""
 
+
+def median(input_list):
+    sorted_list = sorted(input_list)
+    if len(input_list) % 2 == 1:
+        return sorted_list[(len(sorted_list) - 1) / 2]
+    else:
+        first_pos = len(sorted_list) // 2 - 1
+        return (sorted_list[first_pos] +
+                sorted_list[first_pos + 1]) / 2
+
+print(median([4, 5, 5, 4]))
