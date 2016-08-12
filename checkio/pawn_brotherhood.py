c0d2fecc9dbb1af41ec011db93b8173d7bed21ac
CCortello <cody.cortello@gmail.com>
2016-08-12 17:36:50 -0600

    finished pawn_brotherhood =)

--- checkio/pawn_brotherhood.py
+++ checkio/pawn_brotherhood.py
@@ -5,13 +5,24 @@
 
 
 def safe_pawns(pawns):
+
+    """"Given a series of strings in chess notation ('b4, c8', ...) find the
+    number of safe pawns if they were placed in the given locations."""
+
+    # convert given strings into a 2D array
     board = [[0]*8 for i in range(8)]
     for i in pawns:
-        x = ord(i[0]) - 97
-        y = int(i[1]) - 1
-        board[x][y] = 1
-    print(board)
-    return 0
+        board[ord(i[0]) - 97][int(i[1]) - 1] = 1
+
+    # check
+    count = 0
+    for i in pawns:
+        left_defender = chr(ord(i[0]) - 1) + str(int(i[1]) - 1)
+        right_defender = chr(ord(i[0]) + 1) + str(int(i[1]) - 1)
+        print(left_defender, i, right_defender)
+        if right_defender in pawns or left_defender in pawns:
+            count += 1
+    return count
 
 
 print(safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"}))
